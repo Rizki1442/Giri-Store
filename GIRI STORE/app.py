1fc8b07e9deee0a39df1aa5cbdb7e5dd167c3107
Rizki1442 <117267456+Rizki1442@users.noreply.github.com>
2025-02-12 07:24:06 -0800

    Add files via upload

---
+++ GIRI STORE/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='templates', static_folder='static')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/product')
+def product():
+    return render_template('product.html')
+
+@app.route('/lokasi')
+def lokasi():
+    return render_template('lokasi.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
